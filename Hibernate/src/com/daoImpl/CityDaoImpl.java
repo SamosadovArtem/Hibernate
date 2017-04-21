9700862c047e202d8a740c9d17c1313861d9fd37
TP1611013 <TP1611013@1301078-NB.wistronits.com>
2017-04-21 18:03:40 +0800

    commit

--- Hibernate/src/com/daoImpl/CityDaoImpl.java
+++ Hibernate/src/com/daoImpl/CityDaoImpl.java
@@ -38,6 +38,6 @@ public class CityDaoImpl implements ICityDao{
 		for(CityVO vo:list){
 			System.out.println(vo.getCityName());
 		}
-		System.out.println("master");
+		System.out.println("branch");
 	}
 }
