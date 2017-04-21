f01c525d5e22df8ac763b10ad46f54200cbb4cc9
TP1611013 <TP1611013@1301078-NB.wistronits.com>
2017-04-21 14:47:19 +0800

    Update master

--- Hibernate/src/com/daoImpl/CityDaoImpl.java
+++ Hibernate/src/com/daoImpl/CityDaoImpl.java
@@ -38,6 +38,6 @@ public class CityDaoImpl implements ICityDao{
 		for(CityVO vo:list){
 			System.out.println(vo.getCityName());
 		}
-		System.out.println("Local master");
+		System.out.println("Local master2");
 	}
 }
